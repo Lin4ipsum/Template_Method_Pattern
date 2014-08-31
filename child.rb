b36d20fce20ea3eda61c1c67541ba47f1370c510
Lin4ipsum <lin.m.gonzalez@gmail.com>
2014-08-31 10:27:35 -0400

    create template method pattern

---
+++ child.rb
@@ -0,0 +1,22 @@
+class Baby < Human
+
+	def output_speak
+		puts "goo-goo gaa-gaa"
+	end
+
+	def output_gender
+		puts "I am boy or girl"
+	end
+
+	def output_height
+		puts "I'm less than 2 feet"
+	end
+
+	def output_food_preference
+		puts "I eat milk and mushy things"
+	end
+
+	def output_age
+		puts "I'm less than 3 years old"
+	end
+end
---
+++ human.rb
@@ -0,0 +1,29 @@
+class Human
+	def initialize
+		@speak = "I am alive!"
+	end
+
+	def output_human
+		puts output_speak
+		output_age
+		output_gender
+		output_height
+		output_food_preference
+	end
+
+	def output_speak
+		@speak
+	end
+
+	def output_age
+	end
+
+	def output_gender
+	end
+
+	def output_height
+	end
+
+	def output_food_preference
+	end
+end
